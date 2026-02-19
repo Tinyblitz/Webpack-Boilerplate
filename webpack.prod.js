1cd81f048b195c822fecd8d64d0bbf254127bec5
Daniel Tsiu <35878710+Tinyblitz@users.noreply.github.com>
2026-02-19 17:55:52 -0500

    Update webpack.prod.js
    Reduce redundancy in webpack config using webpack merge.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,33 +1,6 @@
-import path from "node:path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+ import { merge } from 'webpack-merge';
+ import common from './webpack.common.js';
 
-export default {
-  mode: "production",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(import.meta.dirname, "dist"),
-    clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-};
+ export default merge(common, {
+   mode: 'production',
+ });
